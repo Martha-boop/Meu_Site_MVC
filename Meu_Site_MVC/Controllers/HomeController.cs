39471630eb8512ea893e09a9ff89229d378853b0
Marta Neris <78042453+Martha-boop@users.noreply.github.com>
2022-03-13 15:20:59 -0300

    Add project files.

---
+++ Meu_Site_MVC/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Meu_Site_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Meu_Site_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+
+        public IActionResult Index()
+        {
+            HomeModel home = new HomeModel();
+            home.Nome = "Marta Neris";
+            home.Email = "martaneris35@gmail.com";
+            
+
+
+            return View(home);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
